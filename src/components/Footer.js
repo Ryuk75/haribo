b85c03211699dcbde6f565ee80c9eff8d72f6f5d
RogerReinosoGomez <info@eniun.com>
2021-11-24 18:21:10 -0500

    24-11-21 18:21

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import twitter from '../img/iconos/twitter.png'
+import facebook from '../img/iconos/facebook.png'
+import instagram from '../img/iconos/instagram.png'
+import gummyBlack from '../img/gummyBlack.png'
+
+const Footer = () => {
+    return(
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
+            <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+            <img className="bi" src={gummyBlack} alt='log' width="24" height="24" />
+            </a>
+            <span className="text-muted">&reg; 2021 Haribo Gummies. Miami, Fl</span>
+        </div>
+
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li className="ms-3"><a className="text-muted" href="none"><img className="bi" src={facebook} alt='log' width="24" height="24" /></a></li>
+            <li className="ms-3"><a className="text-muted" href="none"><img className="bi" src={instagram} alt='log' width="24" height="24" /></a></li>
+            <li className="ms-3"><a className="text-muted" href="none"><img className="bi" src={twitter} alt='log' width="24" height="24" /></a></li>
+        </ul>
+        </footer>
+    );
+}
+
+export default Footer;
