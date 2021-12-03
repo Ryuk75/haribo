cf1dc9aae024b5152ab61c7824f0d7abd3c1f5bf
RogerReinosoGomez <info@eniun.com>
2021-12-02 20:43:11 -0500

    02/12/2021

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import { Fragment } from 'react/cjs/react.production.min';
 //import './App.css';
 
 function App() {
+
+  let information = {
+    location: "2021 Haribo Gummies. Miami, Fl"
+  };
+
   return (
-    <div class='container'>
-      <NavBar />
-      <main className="px-5 text-center">
-          <h1>Cover your page.</h1>
-          <p className="lead">Cover is a one-page template for building simple and beautiful home pages.
-                              Download, edit the text, and add your own fullscreen background photo to
-                              make it your own.
-          </p>
-          <p className="lead">
-              <a href="none" className="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
-          </p>
-      </main>
-      <Footer />
-    </div>
+    <Fragment>
+      <div class='container'>
+        <NavBar />
+        <main className="px-5 text-center">
+            <h1>Cover your page.</h1>
+            <p className="lead">Cover is a one-page template for building simple and beautiful home pages.
+                                Download, edit the text, and add your own fullscreen background photo to
+                                make it your own.
+            </p>
+            <p className="lead">
+                <a href="none" className="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+            </p>
+        </main>
+        <Footer { ...information }/>
+      </div>
+    </Fragment>
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,30 +3,30 @@ import facebook from '../img/iconos/facebook.png'
 import instagram from '../img/iconos/instagram.png'
 import gummyBlack from '../img/gummyBlack.png'
 
-let Footer = () => {
+let Footer = (information) => {
     return(
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div className="col-md-4 d-flex align-items-center">
             <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-            <img className="bi" src={gummyBlack} alt='log' width="24" height="24" />
+            <img className="bi" src={ gummyBlack } alt='log' width="24" height="24" />
             </a>
-            <span className="text-muted">&reg; 2021 Haribo Gummies. Miami, Fl</span>
+            <span className="text-muted">&reg; { information.location }</span>
         </div>
 
         <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
             <li className="ms-3">
                 <a className="text-muted" href="none">
-                    <img className="bi" src={facebook} alt='facebook' width="24" height="24" />
+                    <img className="bi" src={ facebook } alt='facebook' width="24" height="24" />
                 </a>
             </li>
             <li className="ms-3">
                 <a className="text-muted" href="none">
-                    <img className="bi" src={instagram} alt='instagram' width="24" height="24" />
+                    <img className="bi" src={ instagram } alt='instagram' width="24" height="24" />
                 </a>
             </li>
             <li className="ms-3">
                 <a className="text-muted" href="none">
-                    <img className="bi" src={twitter} alt='twitter' width="24" height="24" />
+                    <img className="bi" src={ twitter } alt='twitter' width="24" height="24" />
                 </a>
             </li>
         </ul>
